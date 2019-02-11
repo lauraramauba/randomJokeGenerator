602b739233e02b4abee6f1e44af11039f71ebd5b
Laura Ramírez <lauraramauba@gmail.com>
2019-02-11 12:14:41 +0100

    init

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('#joke-button').click(function() {
+
+    $('#placeholder').remove();
+
+    $.getJSON( 'http://api.icndb.com/jokes/random', function(data) {
+        $('#joke-display').html('<p>'+ data.value.joke + '</p>');
+    });
+
+});
+
