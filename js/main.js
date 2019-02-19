7e0ce8062846479a10334b8f89e1e4bd14589a36
Laura Ramírez <lauraramauba@gmail.com>
2019-02-19 11:32:51 +0100

    no message

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $('#joke-button').click(function() {
 
     $('#placeholder').remove();
-
     $.getJSON( 'http://api.icndb.com/jokes/random', function(data) {
         $('#joke-display').html('<p>'+ data.value.joke + '</p>');
     });
