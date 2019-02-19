fc56e9751ea1ae78355d1d515c9ad7ca801b0c7b
Laura Ramírez <lauraramauba@gmail.com>
2019-02-19 11:37:14 +0100

    gh-pages update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 $('#joke-button').click(function() {
 
     $('#placeholder').remove();
-    $.getJSON( 'https://api.icndb.com/jokes/random', function(data) {
+
+    $.getJSON('https://api.icndb.com/jokes/random', function(data) {
         $('#joke-display').html('<p>'+ data.value.joke + '</p>');
     });
 
