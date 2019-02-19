e20bd0fac02fd983fdf5c7585fc3976bd7ad06ef
Laura Ramírez <lauraramauba@gmail.com>
2019-02-19 11:30:23 +0100

    Updated link

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $('#joke-button').click(function() {
 
     $('#placeholder').remove();
 
-    $.getJSON( 'http://api.icndb.com/jokes/random', function(data) {
+    $.getJSON( 'https://api.icndb.com/jokes/random', function(data) {
         $('#joke-display').html('<p>'+ data.value.joke + '</p>');
     });
 
